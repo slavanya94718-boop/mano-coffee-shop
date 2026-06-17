3f364cf12e44e6548f8968cee300bda912cb30e6
lavanya S <slavanya94718@gmail.com>
2026-06-17 14:34:21 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
+
+
+
+
+
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB Connected Successfully to:', process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.error('❌ DB Connection Error:', error.message);
-    // process.exit(1);   // ← ithai comment pannu first time test ku
+    console.log("❌ DB Connection Error:", error.message);
+    process.exit(1);
   }
 };
 
