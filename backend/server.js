9f983be76dde819c645ed2ddc990a29c020ed0dd
lavanya S <slavanya94718@gmail.com>
2026-06-17 14:33:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
-require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 
-dotenv.config();
+
+
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
-// DB Connection + Server Start
 const startServer = async () => {
   try {
+    console.log("Mongo URI:", process.env.MONGODB_URI);
+
     await connectDB();
+
     const PORT = process.env.PORT || 5000;
+
     app.listen(PORT, () => {
       console.log(`✅ Server running on http://localhost:${PORT}`);
     });
   } catch (err) {
-    console.error('❌ Server failed to start:', err.message);
+    console.error("❌ Server failed to start:", err.message);
   }
 };
 
