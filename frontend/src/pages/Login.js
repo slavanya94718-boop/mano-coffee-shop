f92a76c486de6eb727d642e3dcfb3d205747d908
lavanya S <slavanya94718@gmail.com>
2026-06-17 14:48:57 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -19,7 +19,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post('https://mano-coffee-shop-1.onrender.com//api/auth/login', {
         email,
         password
       });
